93e8714e2fca0e3a0867a577c2d7fc87e1a1244f
Katy <kma2@wellesley.edu>
2016-11-01 19:40:35 -0400

    fixed merge conflicts

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -1,44 +1,39 @@
 import React from 'react';
+import {Grid, Row, Col, Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
 
-import {Grid, Row, Col, FormGroup, ControlLabel, FormControl, form} from 'react-bootstrap';
+// component for users to log into their accounts
 
 export default () => (
   <Grid>
 		<Row className="show-grid">
-			<Col xs={4}>
-				<p> Email </p>
-				<p> Password </p>
-			</Col>
 
-      <form>
-        <FormGroup
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel> Email </ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder="Enter email"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
+      <h1> Sign In </h1>
 
-        <FormGroup
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-          <ControlLabel> Password </ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder="Enter password"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
-      </form>
+			<Col xs={6}>
+        <Form horizontal>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col componentClass={ControlLabel} sm={2}> Email </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup controlId="formHorizontalPassword">
+            <Col componentClass={ControlLabel} sm={2}> Password </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button type="submit"> Sign in </Button>
+              <p> Dont have an account? Create one!  </p>
+            </Col>
+          </FormGroup>
+        </Form>
+      </Col>
+
 		</Row>
 	</Grid>
 )
