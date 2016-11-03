e678da995e19f6052ece0912250d462206a300d7
Katy <kma2@wellesley.edu>
2016-11-03 11:43:35 -0400

    added quantity selection to single product component

--- app/components/SingleProduct.js
+++ app/components/SingleProduct.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Grid, Row, Col, Button} from 'react-bootstrap';
+import {Grid, Row, Col, Button, ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
 
 // use react-rater to implement star ratings
 // TO DO: add rating stars
@@ -12,18 +12,41 @@ export default () => (
 
 				<Col xs={12}>
 					<Col xs={12} md={1}></Col>
+
+					{/* product detail */}
 					<Col xs={12} md={4}>
 						<img src="product_photos/parisian-pineapples.jpg" className="product_image" />
 					</Col>
 					<Col xs={12} md={1}></Col>
 					<Col xs={12} md={4}>
-						<p className="product_name"> Parisian Pineapples </p>
-						<p className="product_price">$6.5</p>
-						<p>From Paris with love, these darling baby pineapples are made with fresh pineapple purée for an all-natural tropical treat.</p>
-						<Col xs={6} className="add_to_bag">
-							<Button block>Add to bag</Button>
+						<Col xs={12}>
+							<p className="product_name"> Parisian Pineapples </p>
+							<p className="product_price">$6.5</p>
+							<p>From Paris with love, these darling baby pineapples are made with fresh pineapple purée for an all-natural tropical treat.</p>
+						</Col>
+						<Col xs={12}>
+							<p> Quantity </p>
+						</Col>
+						<Col xs={12}>
+							<ButtonToolbar>
+					      <DropdownButton title="1" id="dropdown-size-medium">
+					        <MenuItem eventKey="1">2</MenuItem>
+					        <MenuItem eventKey="2">3</MenuItem>
+					        <MenuItem eventKey="3">4</MenuItem>
+									<MenuItem eventKey="3">5</MenuItem>
+					      </DropdownButton>
+					    </ButtonToolbar>
+						</Col>
+						<Col xs={7} className="add_to_cart">
+							<Button block>Add to Cart</Button>
 						</Col>
 					</Col>
+
+					{/* product reviews */}
+					<Col xs={12}>
+						
+					</Col>
+
 				</Col>
 			</Row>
 		</Grid>
