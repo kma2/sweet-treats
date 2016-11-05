c8d46649da2bbf7d986cd7e445c0695335dd0bfa
Katy <kma2@wellesley.edu>
2016-11-05 12:08:45 -0400

    cleaned up code on routes

--- server/api.js
+++ server/api.js
@@ -6,37 +6,30 @@ const Candy = require('APP/db/models/candy')
 const Order = require('APP/db/models/order')
 const User = require('APP/db/models/user')
 const bcrypt = require('bcrypt')
-// const fakeCandy = {name: 'Test', short_description: 'Test', description: 'Test', price: 7.5, quantity: 500, tags: [], numOrdered: 0, status: 'Available', rating: 0, review: [], photo: '../Test/Test'}
-
-//ROUTES TO DO 
-/*
-  Admin routes 
-  User routes 
-  Order routes 
-*/ 
 
 api
   .get('/heartbeat', (req, res) => res.send({ok: true,}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
-  //Adding a candy to an order
+
+  /*----------CANDY ROUTES----------*/
+
+  // adding a candy to an order
   .post('/candy/:id',(req,res,next) => {
     Order.findById(1)
     .then(order =>{
       Candy.findById(req.params.id)
       .then(candy =>{
-        //Now we have candy and order 
-        //Time to associate them 
-        //candy meet order, order meet candy 
         order.addCandy(candy)
-        res.sendStatus(204)
+        res.sendStatus(200)
       })
     }).catch((err) =>{
       console.log("Couldn't find Candy/Order")
       next(err)
     })
   })
-  //delete a specific candy from an order
+
+  // delete a specific candy from an order
   .delete('/candy/:id',(req,res,next) =>{
     Order.findById(1)
     .then(order =>{
@@ -50,6 +43,7 @@ api
       next(err)
     })
   })
+
   //Update a specific candy in an order
   // .put('/candy',(req,res) =>{
   //   Order.findById(1)
@@ -57,6 +51,8 @@ api
   //     order.setCandy(Candy,req.body)
   //   })
   // })
+
+  // get all candies
   .get('/candy',(req,res) =>{
     Candy.findAll({})
     .then((candies) =>{
@@ -64,6 +60,7 @@ api
     })
   })
 
+  // get candy by id
   .get('/candy/:id',(req,res) =>{
     Candy.findById(req.params.id)
     .then((candy) =>{
@@ -72,126 +69,111 @@ api
   })
 
 
-  //logout
+  /*----------USER ROUTES----------*/
+
+  // user can logout
    .get('/user/logout',(req,res) =>{
      req.session = null
      res.sendStatus(204)
-  }) 
-  //Get a user by Id (NEEDS TO BE UPDATED TO INCLUDE AUTH)
+  })
+
+  // get a user by Id (NEEDS TO BE UPDATED TO INCLUDE AUTH)
   .get('/user/:id',(req,res) =>{
     User.findById(req.params.id)
     .then(user =>{
       res.send(user)
-    }) 
+    })
   })
-  
-  //USER 
-  //Update a user 
- 
-  .put('/user/:id',(req,res) =>{ 
+
+  // update a user
+  .put('/user/:id',(req,res) =>{
     User.update(req.body,{
       where:{
         id: req.params.id
       },
       returning: true
     }).then(() =>{
-      res.sendStatus(204)
+      res.sendStatus(200)
     })
   })
-  //Delete a user 
-  .delete('/user/:id',(req,res) =>{ 
+
+  // delete a user
+  .delete('/user/:id',(req,res) =>{
     User.findById(req.params.id)
     .then(user =>{
       user.destroy()
     }).then(user =>{
-      console.log("DELETED ",user)
       res.sendStatus(204)
     })
   })
-  //Register them users bruh
+
+  // register a new user
   .post('/user/register',(req,res) =>{
     User.create(req.body)
     .then(user =>{
-      req.session.user = user 
-      console.log("created user")
-      console.log('session',req.session)
-      // res.sendStatus(204)
-      res.send(user)
+      req.session.user = user
+      res.status(201).send(user)
     })
   })
-  //Log in 
-  .post('/user/login',(req,res) =>{
+
+  // user can login
+  .post('/user/login',(req,res) => {
     User.findOne({
       where:{
         email:req.body.email
       }
-    }).then(user =>{
-      //Auth
+    }).then(user => {
       user.authenticate(req.body.password)
-      .then((binary) =>{
-        if(binary){
+      .then((binary) => {
+        if(binary) {
           console.log(req.body.password)
           console.log('logged in')
-          req.session.user = user 
+          req.session.user = user
           res.sendStatus(200)
         }
-        else{
-          res.sendStatus(404)
+        else {
+          res.sendStatus(401)
         }
       })
-    }).catch(err =>{
-      //Redirects
-      console.log('wrong password m8')
-      res.sendStatus(401)
+    }).catch(err => {
+      // NEED TO UPDATE TO REDIRECT AND SEND USER A MESSAGE
+      res.sendStatus(403)
     })
   })
 
-  //Order 
-  //Get order
+
+  /*----------ORDER ROUTES----------*/
+
+  // get an order
   .get('/order/:id',(req,res) =>{
     Order.findById(req.params.id)
     .then(order =>{
       res.send(order)
     })
-  }) 
-  
-  //Update order
+  })
+
+  // update an order
   .put('/order/:id',(req,res) =>{
     Order.findById(req.params.id)
     .then(order =>{
       order.update(req.body)
     })
-  }) 
+  })
 
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   /*----------ADMIN ROUTES----------*/
-  /*
-    ADMIN stuff 
-      - Add candy to store tick
-      - Set candy status to new status tick 
-      - RUD Users  
-  */
-  //CANDY
-  .post('/admin/candy',(req,res) =>{ 
+
+  // create a new candy
+  .post('/admin/candy',(req,res) =>{
     Candy.create(req.body)
     .then((candy) =>{
       console.log("Created candy ",candy.name)
-      res.sendStatus(204)
+      res.sendStatus(201)
     })
   })
-  .put('/admin/candy/:id/:status',(req,res) =>{ 
+
+  // update a candy's status
+  .put('/admin/candy/:id/:status',(req,res) =>{
     Candy.findById(req.params.id)
     .then((candy) =>{
       candy.update({status:req.params.status})
@@ -200,8 +182,9 @@ api
       res.sendStatus(204)
     })
   })
-  //USER
-  .put('/admin/candy/:id/:status',(req,res) =>{ 
+
+  // update a user's status
+  .put('/admin/candy/:id/:status',(req,res) =>{
     User.findById(req.params.id)
     .then(user =>{
       user.update({status:req.params.status})
@@ -210,7 +193,9 @@ api
       res.sendStatus(204)
     })
   })
-  .delete('/admin/user/:id',(req,res) =>{ 
+
+  // delete a user
+  .delete('/admin/user/:id',(req,res) =>{
     User.findById(req.params.id)
     .then(user =>{
       user.destroy()
@@ -220,13 +205,11 @@ api
     })
   })
 
-  
+
 // Send along any errors
 api.use((err, req, res, next) => {
   res.status(500).send(err)
-}) 
+})
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
-
-// {"name": "Test", "short_description": "Test", "description": "Test", "price": 7.5, "quantity": 500, "tags": [], "numOrdered": 0, "status": "Available", "rating": 0, "review": [], "photo": "../Test/Test""}
--- server/start.js
+++ server/start.js
@@ -17,7 +17,7 @@ const app = express()
 if (!pkg.isProduction) {
   // Logging middleware (dev & testing only)
   app.use(require('volleyball'))
-}  
+}
 
 module.exports = app
   // We'll store the whole session in a cookie
@@ -33,7 +33,7 @@ module.exports = app
   // Authentication middleware
   .use(passport.initialize())
   .use(passport.session())
-  
+
   // Serve static files from ../public
   .use(express.static(resolve(__dirname, '..', 'public')))
 
@@ -45,12 +45,11 @@ module.exports = app
 
 if (module === require.main) {
   // Start listening only if we're the main module.
-  // 
   // https://nodejs.org/api/modules.html#modules_accessing_the_main_module
   const server = app.listen(
     process.env.PORT || 1337,
     () => {
-      console.log(`--- Started HTTP Server for ${pkg.name} ---`)      
+      console.log(`--- Started HTTP Server for ${pkg.name} ---`)
       console.log(`Listening on ${JSON.stringify(server.address())}`)
     }
   )
