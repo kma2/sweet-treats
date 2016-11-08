8ad5da96a318b79703cde97e4b97bc4c3af528a3
Katy <kma2@wellesley.edu>
2016-11-07 20:08:10 -0500

    sign in works

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Grid, Row, Col, Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
+import { Grid, Row, Col, Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 import axios from 'axios';
 // component for users to log into their accounts
 
 export default class Signin extends React.Component {
-    
+
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
@@ -14,9 +14,9 @@ export default class Signin extends React.Component {
     return (
       <Grid>
         <Row className="show-grid">
-  
+
           <h2> Sign In </h2>
-  
+
           <Col xs={6}>
             <Form horizontal onSubmit={this.onSubmit}>
               <FormGroup controlId="formHorizontalEmail">
@@ -25,22 +25,23 @@ export default class Signin extends React.Component {
                   <FormControl name="email" type="email" placeholder="Email" />
                 </Col>
               </FormGroup>
-  
+
               <FormGroup controlId="formHorizontalPassword">
                 <Col componentClass={ControlLabel} sm={2}> Password </Col>
                 <Col sm={10}>
                   <FormControl name="password" type="password" placeholder="Password" />
                 </Col>
               </FormGroup>
-  
+
               <FormGroup>
                 <Col smOffset={2} sm={10}>
                   <Button type="submit"> Sign in </Button>
-                  <p> Dont have an account? Create one!  </p>
+                  <p> Dont have an account? Create one! </p>
                 </Col>
               </FormGroup>
             </Form>
           </Col>
+
         </Row>
       </Grid>
     )
