6fbf364ec630b858f17548e3b84840703c42a221
Christopher Manahan <chris.a.manahan@gmail.com>
2016-11-07 19:43:33 -0500

    working on signin

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -1,39 +1,59 @@
 import React from 'react';
 import {Grid, Row, Col, Form, FormGroup, FormControl, ControlLabel, Button} from 'react-bootstrap';
-
+import axios from 'axios';
 // component for users to log into their accounts
 
-export default () => (
-  <Grid>
-		<Row className="show-grid">
+export default class Signin extends React.Component {
+    
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-      <h2> Sign In </h2>
+  render () {
+    return (
+      <Grid>
+        <Row className="show-grid">
+  
+          <h2> Sign In </h2>
+  
+          <Col xs={6}>
+            <Form horizontal onSubmit={this.onSubmit}>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}> Email </Col>
+                <Col sm={10}>
+                  <FormControl name="email" type="email" placeholder="Email" />
+                </Col>
+              </FormGroup>
+  
+              <FormGroup controlId="formHorizontalPassword">
+                <Col componentClass={ControlLabel} sm={2}> Password </Col>
+                <Col sm={10}>
+                  <FormControl name="password" type="password" placeholder="Password" />
+                </Col>
+              </FormGroup>
+  
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Button type="submit"> Sign in </Button>
+                  <p> Dont have an account? Create one!  </p>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
 
-			<Col xs={6}>
-        <Form horizontal>
-          <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={2}> Email </Col>
-            <Col sm={10}>
-              <FormControl type="email" placeholder="Email" />
-            </Col>
-          </FormGroup>
+  onSubmit(event) {
+    event.preventDefault();
+    const credentials = {
+      email: event.target.email.value,
+      password: event.target.password.value
+    }
 
-          <FormGroup controlId="formHorizontalPassword">
-            <Col componentClass={ControlLabel} sm={2}> Password </Col>
-            <Col sm={10}>
-              <FormControl type="password" placeholder="Password" />
-            </Col>
-          </FormGroup>
-
-          <FormGroup>
-            <Col smOffset={2} sm={10}>
-              <Button type="submit"> Sign in </Button>
-              <p> Dont have an account? Create one!  </p>
-            </Col>
-          </FormGroup>
-        </Form>
-      </Col>
-
-		</Row>
-	</Grid>
-)
+    axios.post('/api/user/login', credentials)
+    .catch(err => console.error(err));
+  }
+}
