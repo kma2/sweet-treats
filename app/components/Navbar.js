c13b938b541dd4f76320f4b3ddd79f0b3dc6b19f
Brandon Culp <bculp513@gmail.com>
2016-11-08 10:59:24 -0500

    login btn text changes in navbar when logged in

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import {Navbar, NavItem, MenuItem, Nav, NavDropdown, ButtonGroup, Button, DropdownButton} from 'react-bootstrap';
+import axios from 'axios';
 
 export default class NavbarComponent extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {loggedIn: false}
+	}
+
+	componentDidMount() {
+		axios.get('api/checkLogin')
+		.then(res => res.data)
+		.then(status => {
+			if (status === "logged in") {
+				this.setState({loggedIn: true})
+			}
+			else {
+				this.setState({loggedIn: false})
+			}
+		})
+
 	}
 
 	render() {
@@ -17,7 +33,7 @@ export default class NavbarComponent extends React.Component {
 			    </Navbar.Header>
 			    <Navbar.Collapse>
 			      <Nav pullRight>
-			        <NavItem eventKey={1} href="#">Sign in</NavItem>
+			        <NavItem eventKey={1} href="#">{this.state.loggedIn ? "Logout" : "Log In"}</NavItem>
 			        <NavItem eventKey={2} href="#">Shopping Cart (1)</NavItem>
 			      </Nav>
 			    </Navbar.Collapse>
